65df6d6b536bc9c089771fee899bcba1180eb276
struong <45595406+struong@users.noreply.github.com>
2021-10-25 14:49:26 +0100

    pi up to 5 dp

--- src/main/scala/Approximate.scala
+++ src/main/scala/Approximate.scala
@@ -1,4 +1,26 @@
-object Approximate {
-  def calculatePi(precision: Int): Double = 3.1
-}
+import scala.annotation.tailrec
 
+object Approximate {
+
+  def doRounding(precision: Int, value: Double) = {
+    BigDecimal(value).setScale(precision, BigDecimal.RoundingMode.HALF_UP).toDouble
+  }
+
+  def calculatePi(precision: Int): Double = {
+    val desiredPi = doRounding(precision, Math.PI)
+
+    @tailrec
+    def leibniz(iteration: Int, result: Double): Double = {
+      val accum = result + math.pow(-1.0, iteration) * (1.0 / ((2.0 * iteration) + 1.0))
+      if(desiredPi == doRounding(precision, accum * 4.0)) {
+        println(s"iteration = ${iteration}")
+        accum * 4.0
+      }
+      else {
+        leibniz(iteration + 1, accum)
+      }
+    }
+
+    leibniz(0, 0.0)
+  }
+}
